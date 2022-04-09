623ebe20e1711b96e6e63ace7f52ddbf84335ad2
Александра Плаксина <plaksinaaleksandra943@gmail.com>
2022-04-10 00:55:37 +0400

    still...

--- SUBD/SUBD/FormPosts.Designer.cs
+++ SUBD/SUBD/FormPosts.Designer.cs
@@ -29,19 +29,84 @@ namespace SUBD
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.ButtonAdd = new System.Windows.Forms.Button();
+			this.ButtonUpd = new System.Windows.Forms.Button();
+			this.ButtonDel = new System.Windows.Forms.Button();
+			this.ButtonRef = new System.Windows.Forms.Button();
+			this.dataGridView = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// ButtonAdd
+			// 
+			this.ButtonAdd.Location = new System.Drawing.Point(388, 24);
+			this.ButtonAdd.Name = "ButtonAdd";
+			this.ButtonAdd.Size = new System.Drawing.Size(85, 31);
+			this.ButtonAdd.TabIndex = 1;
+			this.ButtonAdd.Text = "Добавить";
+			this.ButtonAdd.UseVisualStyleBackColor = true;
+			this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+			// 
+			// ButtonUpd
+			// 
+			this.ButtonUpd.Location = new System.Drawing.Point(388, 73);
+			this.ButtonUpd.Name = "ButtonUpd";
+			this.ButtonUpd.Size = new System.Drawing.Size(85, 31);
+			this.ButtonUpd.TabIndex = 2;
+			this.ButtonUpd.Text = "Изменить";
+			this.ButtonUpd.UseVisualStyleBackColor = true;
+			// 
+			// ButtonDel
+			// 
+			this.ButtonDel.Location = new System.Drawing.Point(388, 124);
+			this.ButtonDel.Name = "ButtonDel";
+			this.ButtonDel.Size = new System.Drawing.Size(85, 31);
+			this.ButtonDel.TabIndex = 3;
+			this.ButtonDel.Text = "Удалить";
+			this.ButtonDel.UseVisualStyleBackColor = true;
+			// 
+			// ButtonRef
+			// 
+			this.ButtonRef.Location = new System.Drawing.Point(388, 177);
+			this.ButtonRef.Name = "ButtonRef";
+			this.ButtonRef.Size = new System.Drawing.Size(85, 31);
+			this.ButtonRef.TabIndex = 4;
+			this.ButtonRef.Text = "Обновить";
+			this.ButtonRef.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView
+			// 
+			this.dataGridView.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+			this.dataGridView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView.ColumnHeadersVisible = false;
+			this.dataGridView.Location = new System.Drawing.Point(12, 15);
+			this.dataGridView.Name = "dataGridView";
+			this.dataGridView.Size = new System.Drawing.Size(313, 308);
+			this.dataGridView.TabIndex = 5;
 			// 
 			// FormPosts
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(490, 344);
+			this.Controls.Add(this.dataGridView);
+			this.Controls.Add(this.ButtonRef);
+			this.Controls.Add(this.ButtonDel);
+			this.Controls.Add(this.ButtonUpd);
+			this.Controls.Add(this.ButtonAdd);
 			this.Name = "FormPosts";
 			this.Text = "Должности";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+		private System.Windows.Forms.Button ButtonAdd;
+		private System.Windows.Forms.Button ButtonUpd;
+		private System.Windows.Forms.Button ButtonDel;
+		private System.Windows.Forms.Button ButtonRef;
+		private System.Windows.Forms.DataGridView dataGridView;
 	}
 }
--- SUBD/SUBD/FormPosts.cs
+++ SUBD/SUBD/FormPosts.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Database.Tables;
+using Database.Implements;
 
 namespace SUBD
 {
 	public partial class FormPosts : Form
 	{
+		PostStorage postStorage;
 		public FormPosts()
 		{
 			InitializeComponent();
+			postStorage = new PostStorage();
 		}
+
+
+
+		private void ButtonAdd_Click(object sender, EventArgs e)
+		{
+			var form = Program.Container.Resolve<FormComponent>();
+			if (form.ShowDialog() == DialogResult.OK)
+			{
+				LoadData();
+			}
+		}
+
 	}
 }
